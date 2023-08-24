40bf81e35a9020677c9a4eab5568c6ae664e3cf9
ClaytonBudd <claytonbudd@gmail.com>
2023-08-24 17:30:44 -0600

    created grid build button, changed buildGrid into function

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
-rowLength = 16
-columnLength = rowLength 
-for (i = 0; i < columnLength; i++ ){
-    const container = document.querySelector('#gridContainer')
-    const row = document.createElement('div')
-    row.setAttribute("id", 'row');
-    container.appendChild(row);
-    for (j = 0; j < rowLength; j++){  
-        rowContainer = row
-        const square = document.createElement('div');
-        square.setAttribute("id", 'square');
-        rowContainer.appendChild(square);
-    }
-}
+const button = document.querySelector('button')
+button.addEventListener("click", buildGrid);
 
-let square = document.querySelectorAll("#square").forEach(elem => {
-    elem.addEventListener("mouseover", item => {
-        elem.setAttribute('style', 'background-color:black;')
-    });
-})
+
+function buildGrid()
+{
+    let parent = document.getElementById("gridContainer")
+    while (parent.hasChildNodes())
+        parent.removeChild(parent.firstChild)
+    let rowLength = prompt("Enter a side length");
+    let columnLength = rowLength;
+    for (i = 0; i < columnLength; i++ ){
+        const container = document.querySelector('#gridContainer');
+        const row = document.createElement('div');
+        row.setAttribute("id", 'row');
+        container.appendChild(row);
+        for (j = 0; j < rowLength; j++){  
+            rowContainer = row
+            const square = document.createElement('div');
+            square.setAttribute("id", 'square');
+            rowContainer.appendChild(square);
+        }
+    }
+    let square = document.querySelectorAll("#square").forEach(elem => {
+        elem.addEventListener("mouseover", item => 
+        {
+            elem.setAttribute('style', 'background-color:black;')
+        });
+    })    
+}
 
 
