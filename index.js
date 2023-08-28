0c4c7ffe095299603d4ff49732ef4f8b74d70ff8
ClaytonBudd <claytonbudd@gmail.com>
2023-08-28 15:01:01 -0600

    cleaned up build and clear functions, function calls to make more sense, added mouse up/down functionality.

--- index.js
+++ index.js
@@ -1,72 +1,53 @@
 const buildButton = document.getElementById('buildGrid')
-buildButton.addEventListener("click", buildGrid);
+buildButton.addEventListener("click", sizePrompt);
 
 const clearButton = document.getElementById('clearGrid')
-clearButton.addEventListener('click', clearGrid);
+clearButton.addEventListener('click', buildGrid);
+
+window.addEventListener("mousedown", () => {
+    isDraw = true;
+    });
+window.addEventListener("mouseup", () => {
+    isDraw = false;
+});   
+
+
+function buildGrid(){
+    clearGrid()
+    for (i = 0; i < columnLength; i++ ){
+        const container = document.querySelector('#gridContainer');
+        const row = document.createElement('div');
+        row.setAttribute("id", 'row');
+        container.appendChild(row);
+        for (j = 0; j < rowLength; j++){  
+            rowContainer = row
+            const square = document.createElement('div');
+            square.setAttribute("id", 'square');
+            rowContainer.appendChild(square);
+        }
+    }draw()   
+};
 
 
 function clearGrid(){
     let parent = document.getElementById("gridContainer")
     while (parent.hasChildNodes())
         parent.removeChild(parent.firstChild)
-    columnLength = rowLength;
-    for (i = 0; i < columnLength; i++ ){
-        const container = document.querySelector('#gridContainer');
-        const row = document.createElement('div');
-        row.setAttribute("id", 'row');
-        container.appendChild(row);
-        for (j = 0; j < rowLength; j++){  
-            rowContainer = row
-            const square = document.createElement('div');
-            square.setAttribute("id", 'square');
-            rowContainer.appendChild(square);
-            }
-        }
-        let square = document.querySelectorAll("#square").forEach(elem => {
-            elem.addEventListener("mouseover", item => 
-            {
-                elem.setAttribute('style', 'background-color:black;')
-            });
-        })    
-    
-}
+};
 
-
-function buildGrid()
-{
-    let parent = document.getElementById("gridContainer")
-    while (parent.hasChildNodes())
-        parent.removeChild(parent.firstChild)
-    let rowLength = 0;    
-    rowLength = sizePrompt()
-    while (rowLength > 100){
-        rowLength = sizePrompt()
-    }
-        let columnLength = rowLength;
-    for (i = 0; i < columnLength; i++ ){
-        const container = document.querySelector('#gridContainer');
-        const row = document.createElement('div');
-        row.setAttribute("id", 'row');
-        container.appendChild(row);
-        for (j = 0; j < rowLength; j++){  
-            rowContainer = row
-            const square = document.createElement('div');
-            square.setAttribute("id", 'square');
-            rowContainer.appendChild(square);
-        }
-    }
-    let square = document.querySelectorAll("#square").forEach(elem => {
+function draw(){
+    document.querySelectorAll("#square").forEach(elem => {
         elem.addEventListener("mouseover", item => 
         {
-            elem.setAttribute('style', 'background-color:black;')
-        });
-    })    
-}
-
-
-function sizePrompt()
-{
+        if(isDraw){elem.setAttribute('style', 'background-color:black;')}
+        }
+    )}
+)};
+                
+function sizePrompt(){
     rowLength = prompt('Enter a side length (max 100)')
-    console.log(rowLength)
-    return rowLength
-}
+    columnLength = rowLength
+    buildGrid()    
+};
+
+
